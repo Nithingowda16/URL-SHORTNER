b35943898ce85cb7838657e60b990a7402bf8caa
Nithin.S <nithin.s491@gmail.com>
2020-03-11 22:47:23 +0530

    Added Clicks counter

--- main.php
+++ main.php
@@ -23,17 +23,23 @@ if (isset($_GET['title'])) {
     $res->execute();
     $goto = $res->get_result()->fetch_array();
     $g = $goto[1];
-    header ("Location: $g");
+    $clicks=$goto[3] + 1;
+    $echo = '<script>console.log("'.$clicks.'")</script>'; 
+    echo $echo;
+    $linkk = 'UPDATE links SET Clicks = '.$clicks.' WHERE title=\''.$_GET['title'].'\'';
+    echo $linkk;
+    $increment = $connect->prepare($linkk);
+    $increment->execute();
+    header("Location: $g");
 }
 if (isset($_POST['submit'])) {
     $title = generateRandomString();
-    
   	if (substr($_POST['textarea'], 0, 4) != "http") {
       $url = "http://".$_POST['textarea'];
     } else {
     $url = $_POST['textarea'];
     }
-	$res = $connect->prepare("INSERT INTO links VALUES('',?,?)");
+	$res = $connect->prepare("INSERT INTO links VALUES('',?,?,'')");
   	$res->bind_param("ss",$url,$title);
   	$res->execute();
     echo "<script>prompt('YOUR SHORTENED URL IS:', '".$siteurl."/".$title."');</script>"; 
